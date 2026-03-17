17d2df10de01aff8f9e45ba5a80505b6cc67bade
pabloserra <58600046+pabloserra@users.noreply.github.com>
2026-03-17 11:26:33 +0100

    Update client.py

--- client/client.py
+++ client/client.py
@@ -6,9 +6,14 @@ socket= socket.socket()
 socket.connect((host,port))
 print("Conectado a su banco virtual")
 while True:
-    input= input()
-    socket.send(input)
+    # Recibir mensaje del servidor
+    data = socket.recv(1024)   # hasta 1024 bytes
+    if data:
+        print("Servidor:", data.decode())
 
+    # Enviar mensaje al servidor
+    entrada = input()
+    socket.send(entrada.encode())
 socket.close()
 
 print("Hasta pronto")
